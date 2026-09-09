ec2d323b7213d2f0ed1b665d81b189b54838f101
hieunghia15 <chunghieunghia1509@gmail.com>
2026-09-09 21:32:02 +0700

    Install Laravel 13

---
+++ resources/js/app.tsx
@@ -0,0 +1,23 @@
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+createInertiaApp({
+    title: (title) => `${title} - Laravel`,
+
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx'),
+        ),
+
+    setup({ el, App, props }) {
+        createRoot(el).render(
+            <App {...props} />,
+        );
+    },
+
+    progress: {
+        color: '#4B5563',
+    },
+});
